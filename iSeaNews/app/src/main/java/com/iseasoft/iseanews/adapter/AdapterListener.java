fb1acc65755ce4283353da616c3389f555a0f157
Pham Van Hai <haivan.qn@gmail.com>
2018-05-20 18:27:19 +0700

    Delete project

--- iSeaNews/app/src/main/java/com/iseasoft/iseanews/adapter/AdapterListener.java
+++
@@ -1,9 +0,0 @@
-package com.iseasoft.iseanews.adapter;
-
-import android.support.v7.widget.RecyclerView;
-
-public interface AdapterListener {
-
-    public void onItemClickListener(Object object, int pos, RecyclerView.ViewHolder holder);
-
-}
